ccd5b3dd37e5601c72c42d3b87ba888674639079
siby <simonlobito2006@gmail.com>
2024-11-19 16:44:56 -0500

    mas mejoras

--- src/componentes/PrimerSeccion.jsx
+++ src/componentes/PrimerSeccion.jsx
@@ -4,12 +4,13 @@ import '../estilos/primer-seccion.css'
 export default function PrimerSeccion() {
   return (
     <div className='primer-seccion'> 
-    <div className='saa'>
-        <h2 className='descuento-25'>25% <br />DESCUENTO</h2>
-        <p>Durante el mes de Agosto en frutas y verduras seleccionadas</p>
-        <button className='compra-ahora'><span>COMPRA AHORA</span></button>
-    </div>
-        <img src="https://png.pngtree.com/png-clipart/20231116/original/pngtree-fruits-and-vegetables-market-photo-png-image_13575958.png" alt="" />
+      <div className='text-container-ps'>
+          <h2 className='descuento-25'>25% <br />DESCUENTO</h2>
+          <p>Durante el mes de Agosto en frutas y verduras seleccionadas</p>
+          <button className='compra-ahora'><span>COMPRA AHORA</span></button>
+      </div>
+      <img className='imagen-ps' src="https://png.pngtree.com/png-clipart/20231116/original/pngtree-fruits-and-vegetables-market-photo-png-image_13575958.png" alt="" />
     </div>
   )
 }
+ 
--- src/componentes/SliderCategorias.jsx
+++ src/componentes/SliderCategorias.jsx
@@ -1,97 +1,52 @@
 import React from 'react';
 import '../estilos/slider-categorias.css';
+import categorias from '../productos/categorias.json';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css"; // Estilos básicos de Swiper
+import "swiper/css/pagination"; // Estilos para paginación
+import "swiper/css/navigation"; // Estilos para navegación
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 export default function SliderCategorias({ setCategoria }) {
 
     const seleccionarCategoria = (category) => {
         setCategoria(category);
-    }
+    };
 
     return (
         <div className='slider-container'>
-            <div className='categorias-container'>
-                <div className='categoria' onClick={() => seleccionarCategoria('Todo')}>
-                    <img src="https://cosechasunicas.com/wp-content/uploads/2023/09/pngegg-3.png" alt="" />
-                    <p>Todo</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Frutas')}>
-                    <img src="https://static.vecteezy.com/system/resources/thumbnails/020/899/515/small/red-apple-isolated-on-white-png.png" alt="" />
-                    <p>Frutas</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Verduras')}>
-                    <img src="https://static.vecteezy.com/system/resources/previews/027/216/344/original/onion-onion-transparent-background-ai-generated-free-png.png" alt="verduras" />
-                    <p>Verduras</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Dulces')}>
-                    <img src="https://static.vecteezy.com/system/resources/previews/028/332/207/non_2x/sweets-candies-and-treats-ai-generative-free-png.png" alt="" />
-                    <p>Dulces</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Carnes')}>
-                    <img src="https://static.vecteezy.com/system/resources/previews/028/332/216/non_2x/fresh-raw-beef-meat-ai-generative-free-png.png" alt="" />
-                    <p>Carnes</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Frutos Secos')}>
-                    <img src="https://static.vecteezy.com/system/resources/previews/012/596/342/non_2x/group-of-almond-nuts-concept-free-png.png" alt="" />
-                    <p>Frutos secos</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Aceites')}>
-                    <img src="https://png.pngtree.com/png-clipart/20240309/original/pngtree-bottle-of-vegetable-oil-png-image_14548258.png" alt="" />
-                    <p>Aceites</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Salsas')}>
-                    <img src="https://goodmorningvietnam.shop/shop/55-large_default/salsa-sriracha-al-peperoncino-piccante-sriracha-hot-chilli-sauce-tuong-ot-sriracha-200ml.jpg" alt="" />
-                    <p>Salsas</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Cereales')}>
-                    <img src="https://superlavioleta.com/cdn/shop/files/avena_granel.png?v=1719498688" alt="" />
-                    <p>Cereales</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Lácteos')}>
-                    <img src="https://static.vecteezy.com/system/resources/thumbnails/019/550/837/small/milk-bottle-mockup-on-transparent-background-file-png.png" alt="" />
-                    <p>Lacteos</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Todo')}>
-                    <img src="https://cosechasunicas.com/wp-content/uploads/2023/09/pngegg-3.png" alt="" />
-                    <p>Todo</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Frutas')}>
-                    <img src="https://static.vecteezy.com/system/resources/thumbnails/020/899/515/small/red-apple-isolated-on-white-png.png" alt="" />
-                    <p>Frutas</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Verduras')}>
-                    <img src="https://static.vecteezy.com/system/resources/previews/027/216/344/original/onion-onion-transparent-background-ai-generated-free-png.png" alt="verduras" />
-                    <p>Verduras</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Dulces')}>
-                    <img src="https://static.vecteezy.com/system/resources/previews/028/332/207/non_2x/sweets-candies-and-treats-ai-generative-free-png.png" alt="" />
-                    <p>Dulces</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Carnes')}>
-                    <img src="https://static.vecteezy.com/system/resources/previews/028/332/216/non_2x/fresh-raw-beef-meat-ai-generative-free-png.png" alt="" />
-                    <p>Carnes</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Frutos Secos')}>
-                    <img src="https://static.vecteezy.com/system/resources/previews/012/596/342/non_2x/group-of-almond-nuts-concept-free-png.png" alt="" />
-                    <p>Frutos secos</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Aceites')}>
-                    <img src="https://png.pngtree.com/png-clipart/20240309/original/pngtree-bottle-of-vegetable-oil-png-image_14548258.png" alt="" />
-                    <p>Aceites</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Salsas')}>
-                    <img src="https://goodmorningvietnam.shop/shop/55-large_default/salsa-sriracha-al-peperoncino-piccante-sriracha-hot-chilli-sauce-tuong-ot-sriracha-200ml.jpg" alt="" />
-                    <p>Salsas</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Cereales')}>
-                    <img src="https://superlavioleta.com/cdn/shop/files/avena_granel.png?v=1719498688" alt="" />
-                    <p>Cereales</p>
-                </div>
-                <div className='categoria' onClick={() => seleccionarCategoria('Lácteos')}>
-                    <img src="https://static.vecteezy.com/system/resources/thumbnails/019/550/837/small/milk-bottle-mockup-on-transparent-background-file-png.png" alt="" />
-                    <p>Lacteos</p>
-                </div>
-            </div>
+            <Swiper
+                modules={[Navigation, Pagination, Autoplay]}
+                spaceBetween={10} // Espaciado entre los elementos
+                slidesPerView={3} // Cantidad de elementos visibles
+                navigation={{
+                    prevEl: '.custom-prev', // Clase del botón previo
+                    nextEl: '.custom-next', // Clase del botón siguiente
+                }}
+                pagination={{ clickable: true }} // Paginación habilitada
+                autoplay={{ delay: 3000 }} // Autoplay configurado con retraso
+                breakpoints={{
+                    // Configuración responsiva
+                    640: { slidesPerView: 2 },
+                    768: { slidesPerView: 3 },
+                    1024: { slidesPerView: 4 },
+                }}
+            >
+                {categorias.map((categoria, index) => (
+                    <SwiperSlide key={index}>
+                        <div
+                            className='categoria'
+                            onClick={() => seleccionarCategoria(categoria.categoria)}
+                        >
+                            <img src={categoria.imagen} alt={categoria.nombre} />
+                            <p>{categoria.nombre}</p>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </div>
     );
 }
+
+
 
